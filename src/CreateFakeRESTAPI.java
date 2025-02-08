559a39b6bcb840483a18fedb91a5effccdc02abc
Ankita-code14 <ankita14shikhare@gmail.com>
2025-02-09 00:45:38 +0530

    File create for Mocking APIs

---
+++ src/CreateFakeRESTAPI.java
@@ -0,0 +1,22 @@
+
+public class CreateFakeRESTAPI {
+
+	//https://github.com/typicode/json-server = Json Server
+	
+	//To check node Version = CMD -> node -v -> npm -v
+	
+	//To Install Json Server = npm install -g json-server
+	
+	//To start Json server = json-server --watch db.json
+	
+	//Add json payload data in db.json file which is available at where you start server
+	
+	//youtube vedio link - https://www.youtube.com/watch?v=7vx0RIwHVzg&list=PLhW3qG5bs-L_dJ0YvpKE0m-VK6WRsXDTp&index=1&pp=iAQB
+	
+	//Free and open source desktop Application for API mocks = https://mockoon.com/
+	
+	//to create unlimited number of APIs with your own data = https://faux-api.com/
+
+
+	
+}
